242315d5ce582b8ba503e8430ebdbeca7a247c79
Aditya <aaditya1392@gmail.com>
2023-03-23 00:05:47 +0530

    name showing now

--- client/client.js
+++ client/client.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     const inputL = document.getElementById('loginInput')
     const loginBtn = document.getElementById('loginbtn')
     const exitBtn = document.getElementById('chatexit')
@@ -7,78 +7,82 @@
     const messageBox = document.querySelector('.message')
 
     const socket = io();
-    
+
     let uname;
 
     const login = () => {
         let username = inputL.value;
         if (username.length === 0) return;
-        socket.emit('newuser',username)
+        socket.emit('newuser', username)
         uname = username;
         document.querySelector(".login").classList.remove('active')
         document.querySelector(".chat").classList.add('active')
     }
     loginBtn.onclick = login
-    document.querySelector('.login').addEventListener('keypress',(e)=>{
-        if(e.key == "Enter") {
+    document.querySelector('.login').addEventListener('keypress', (e) => {
+        if (e.key == "Enter") {
             console.log('loged')
             login()
         }
     })
 
     exitBtn.onclick = () => {
-        socket.emit('exituser',uname)
+        socket.emit('exituser', uname)
         window.location.href = window.location.href
     }
 
 
     const send = () => {
         let m = inputM.value
-        if(m.length > 0) {
-            render("my",m)
-            socket.emit('chat',m)
+        let info = {
+            name: uname,
+            mess: m
+        }
+        if (m.length > 0) {
+            render("my", info)
+            socket.emit('chat', info)
             inputM.value = ""
         }
     }
     sendBtn.onclick = send
-    document.querySelector('.chat').addEventListener('keypress',(e)=>{
-        if(e.key == "Enter") {
+    document.querySelector('.chat').addEventListener('keypress', (e) => {
+        if (e.key == "Enter") {
             console.log('loged')
             send()
         }
     })
 
-    socket.on('update',(update)=>{
-        render('update',update)
+    socket.on('update', (update) => {
+        render('update', update)
     })
 
-    socket.on('chat',(message) => {
-        render('other',message)
+    socket.on('chat', (message) => {
+        render('other', message)
     })
 
 
-    const render = (type,message) => {
-        if(type == "my") {
+    const render = (type, message) => {
+        if (type == "my") {
             let elem = document.createElement('div')
-            elem.setAttribute('class','my')
-            elem.innerHTML = 
-            `
-                <p>${message}</p>
+            elem.setAttribute('class', 'my')
+            elem.innerHTML =
+                `
+                <p>${message.mess}</p>
             `
             messageBox.appendChild(elem)
-        }else if(type == "other") {
+        } else if (type == "other") {
             let elem = document.createElement('div')
-            elem.setAttribute('class','other')
-            elem.innerHTML = 
-            `
-                <p>${message}</p>
+            elem.setAttribute('class', 'other')
+            elem.innerHTML =
+                `
+                <p>${message.name}:${message.mess}</p>
             `
             messageBox.appendChild(elem)
-        }else if(type == "update") {
+        } else if (type == "update") {
             let elem = document.createElement('div')
-            elem.setAttribute('class','update')
-            elem.innerHTML = 
-            `
+            elem.setAttribute('class', 'update')
+            elem.innerHTML =
+                `
                 <p>${message}</p>
             `
             messageBox.appendChild(elem)
